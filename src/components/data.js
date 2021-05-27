dda624f14029fac94bf31f1f50155b86eb770dcd
unknown <rabihutomo11@gmail.com>
2021-05-27 19:55:15 +0700

    remove unnecessary project

--- src/components/data.js
+++ src/components/data.js
@@ -31,46 +31,6 @@ let data = {
 			"pic": ["treot", "treot2", "treot3"]
 		},
 		{
-			"title": "Easy Bank Landing Page ",
-			"tech": ["html", "css", "js"],
-			"desc": " ",
-			"link": {
-				"host": "https://lucid-borg-7ad720.netlify.app/",
-				"github": "https://github.com/RobyCigar/easy-bank"
-			},
-			"pic": ["easy-bank", "easy-bank1"]
-		},
-		{
-			"title": "Bookmark Landing Page",
-			"tech": ["html", "css", "js"],
-			"desc": " ",
-			"link": {
-				"host": "https://6044ea7e350124000726b6a1--fervent-mccarthy-3b48bc.netlify.app/",
-				"github": "https://github.com/RobyCigar/bookmark-landing-page"
-			},
-			"pic": ["bookmark", "bookmark1"]
-		},
-		{
-			"title": "Covid Visualization",
-			"tech": ["react", "api"],
-			"desc": "Displaying covid data using graph",
-			"link": {
-				"host": "https://covid-vizzz-555qbyeg1-robycigar.vercel.app/",
-				"github": "https://github.com/RobyCigar/mental-math"
-			},
-			"pic": ["covid", "covid2"]
-		},
-		{
-			"title": "Loopstudio Landing Page",
-			"tech": ["html", "css", "js"],
-			"desc": "",
-			"link": {
-				"host": "https://festive-albattani-68850b.netlify.app/",
-				"github": "https://github.com/RobyCigar/loopstudios"
-			},
-			"pic": ["loopstudio", "loopstudio1", "loopstudio2"]
-		},
-		{
 			"title": "Mental Math",
 			"tech": ["react-native", "expo"],
 			"desc": "This is my first project with React Native, the main idea of this project is just to implement tutorial that i've learn with my own ideas. Mental Math is just a simple Arithmethic Game apps that can be play in both android and iOS. The rule of this game is simple, the question comes up and user answer the number. The unique part of this game is that the user does not allowed taking the calculator. 😆",
@@ -88,35 +48,6 @@ let data = {
 				"github": "https://github.com/RobyCigar/next.js-blog"
 			},
 			"pic": ["blog1", "blog2"]
-		},	
-		{
-			"title": "Telegram Bot",
-			"tech": ["nodejs", "api"],
-			"desc": "I'm using simi api and telegram api in this project, the main feature is the bot can answer whatever you say, sometimes simi's is rude hahaha. Note: the bot is currently inactive because i use free server.",
-			"link": {
-				"github": "https://github.com/RobyCigar/telegram-bot"
-			},
-			"pic": ["simi"]
-		},
-		{
-			"title": "Neumorphism Dashboard",
-			"tech": ["react"],
-			"desc": " ",
-			"link": {
-				"host": "https://neumorphism-dashboard.vercel.app/",
-				"github": "https://github.com/RobyCigar/neumorphism-dashboard"
-			},
-			"pic": ["neumorphism", "neumorphism2"]
-		},
-		{
-			"title": "React Portofolio",
-			"tech": ["react"],
-			"desc": " ",
-			"link": {
-				"host": "https://rabih.vercel.app/",
-				"github": "https://github.com/RobyCigar/Portfolio"
-			},
-			"pic": ["porto", "porto1"]
 		},
 
 	]
