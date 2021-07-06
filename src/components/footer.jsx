2defb6e6af160f88f9acefdad07e1c8670c039ea
RobyCigar <69680330+RobyCigar@users.noreply.github.com>
2021-07-06 17:53:11 +0700

    improve: delete yt and twit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -14,23 +14,15 @@ function Footer() {
   			<div className={styles.socmed}>
 					<div>
             <img src={insta} alt="Istagram"/>
-            <p className={styles.link}><a href="https://instagram.com/rabih31415">Instagram : @rabih31415</a></p>
+            <p className={styles.link}><a href="https://instagram.com/rabih3.1415">Instagram : @rabih3.1415</a></p>
           </div>
           <div>
             <img src={github} alt="github"/>
             <p className={styles.link}><a href="https://github.com/Robycigar">Github : RobyCigar</a></p>
           </div>
           <div>
-            <img src={twitter} alt="twitter"/>
-            <p className={styles.link}> <a  href="https://twitter.com/Robycigar">Twitter : @RobyCigar</a></p>
-          </div>
-          <div>
             <img src={linkedin} alt="linkedin"/>
             <p className={styles.link}> <a  href="https://www.linkedin.com/in/rabih-utomo-36955a1b9/">Linkedin : Rabih Utomo</a></p>
-          </div>
-          <div>
-            <img src={youtube} alt="youtube"/>
-            <p className={styles.link}> <a href="https://www.youtube.com/channel/UC3QMJzy0nw4MM1t4QHwXr-A">YouTube : Roby Cigar</a></p>
           </div>
 				</div>
         <p className={styles.attr}>Created with 💖 in 🇮🇩</p>
